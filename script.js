feec3f73f65f73c06935905396523a3aa3f4b0f6
Vectorax <rice.jacob@gmail.com>
2022-04-07 10:13:36 -0500

    save point

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 
 
-function createGrid (colums,rows){
+function createGrid (gridSize){
+    let container = document.getElementById('container');
+    let gridQty = gridSize * gridSize;
 
+    for (let i=0; gridQty >= i; i++){
+        let gridBox = document.createElement('div');
+        gridBox.setAttribute('id',i);
+        gridBox.setAttribute('class','gridBox');
+        container.appendChild(gridBox);
+    }
+    
 };
 
-
-
-createGrid(16,16);
